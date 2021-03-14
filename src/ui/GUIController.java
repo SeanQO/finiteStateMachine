77474d2fb217b74fe5f4daa4a7a093ce26d27411
unknown <seanquintero3@gmail.com>
2021-03-13 19:46:11 -0500

    Added add state functionality, now adds a state until gets to state Z wich is the las state currentlyavailable

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -1,5 +1,50 @@
 package ui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class GUIController {
+	@FXML
+	private VBox vbox;
 
+	@FXML
+	private HBox hboxA;
+	
+	private Character lastStateL;
+	
+	public GUIController() {
+		lastStateL = 'A';
+	}
+
+	@FXML
+	void addState(ActionEvent event) {
+		
+		if (lastStateL < 90) {
+			HBox newHBox = new HBox(4);
+			
+			Label stateLetter = new Label((char) (lastStateL + 1) + "");
+			lastStateL = (char) (lastStateL + 1);
+			stateLetter.setStyle("-fx-font: 30 arial;");
+			ComboBox<Character> ceroSuccesor = new ComboBox<>();
+			ComboBox<Character> oneSuccesor = new ComboBox<>();
+			RadioButton acceptance = new RadioButton("Acceptance state");
+			
+			newHBox.getChildren().addAll(stateLetter,ceroSuccesor,oneSuccesor,acceptance);
+			
+			newHBox.setSpacing(hboxA.getSpacing());
+			newHBox.setAlignment(Pos.CENTER);
+			vbox.getChildren().add(newHBox);
+		}
+		
+		
+	}
+
+	
 }
+
+
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,56 @@
 package ui;
 
-public class Main {
+import java.util.Optional;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+  
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("fxml/mainPane.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Login");
+		primaryStage.setResizable(false);
+		
+		//sets an alert for when the user tries to close the program window.
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			
+            public void handle(WindowEvent e) {
+            	Alert alert = new Alert(AlertType.CONFIRMATION);
+        		
+        		alert.setTitle("Exit confirmation");
+        		alert.setHeaderText("");
+        		alert.setContentText("Are you sure you want to exit the program?");
+        		
+        		Optional<ButtonType> result = alert.showAndWait();
+        		
+        		if (result.get() == ButtonType.OK) {
+        			Platform.exit();
+        	        System.exit(0);
+        		}else {
+        			e.consume();
+        		}
+            	
+            }
+        });
+		
+		primaryStage.show();
+
+	}
 }
