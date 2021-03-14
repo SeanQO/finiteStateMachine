930a62e245d5cb6744629f802a2a762a8b835257
unknown <seanquintero3@gmail.com>
2021-03-13 21:16:11 -0500

    fixed bugs, now looks fully functional and no bugs

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -52,7 +52,8 @@ public class GUIController {
 			if (selectedTwo != ' ' && selectedTwo <= lastStateL) {
 				((ComboBox<Character>) ((HBox) vbox.getChildren().get(i)).getChildren().get(2)).setValue(selectedTwo);
 			}
-			
+			selectedOne = ' ';
+			selectedTwo = ' ';
 		}
 		
 	}
